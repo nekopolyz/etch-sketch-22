6ad9b36768f8d6d80d65d4775ea64e00f8813590
Lan <minenie@gmail.com>
2022-03-30 23:03:03 -0700

    add clear button

--- main.js
+++ main.js
@@ -1,26 +1,51 @@
 const container = document.querySelector('.container')
 
 
-function createGrid(size){
+// CreateGrid function
+
+function createGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i =0; i < size * size; i++){
+    for (let i = 0; i < size * size; i++) {
         let div = document.createElement("div");
         div.classList.add('grid')
         container.appendChild(div)
-    
+
     }
+    hover()
+
+}
+createGrid(16)
+
+
+// change color when mouse enter divs
+
+function hover() {
+    grid = document.querySelectorAll('.grid')
+    grid.forEach((grid) => {
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add("hover");
+        });
+    });
 
 }
 
 
-grid = document.querySelectorAll('.grid')
-grid.forEach((grid) => {
-    grid.addEventListener('mouseover', (e)=>{
-        grid.classList.add("hover");
+// clear hover effect 
+
+function clear() {
+    let clear = document.querySelector('.clear')
+    clear.addEventListener('click', () => {
+        grid.forEach((grid) => {
+            grid.classList.remove("hover");
+        });
     });
-});
+
+}
+
+clear()
 
 
-createGrid(16)
+
+
 
